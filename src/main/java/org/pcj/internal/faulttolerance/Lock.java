4dda1b08adb7134d44570e3d4ad17ad218bacd61
Michal Szynkiewicz <michal.szynkiewicz@pointpack.pl>
2015-04-16 15:37:32 +0200

    commented out lock logging + title change for related work

--- src/main/java/org/pcj/internal/faulttolerance/Lock.java
+++ src/main/java/org/pcj/internal/faulttolerance/Lock.java
@@ -1,6 +1,5 @@
 package org.pcj.internal.faulttolerance;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -11,45 +10,45 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class Lock {
     private static final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
-    static ThreadLocal<Integer> localLockCount;
-    static ThreadLocal<Integer> localWriteLockCount = new ThreadLocal<>();
-    static AtomicInteger lockCount = new AtomicInteger(0);
-    static AtomicInteger writeLockCount = new AtomicInteger(0);
+//    static ThreadLocal<Integer> localLockCount;
+//    static ThreadLocal<Integer> localWriteLockCount = new ThreadLocal<>();
+//    static AtomicInteger lockCount = new AtomicInteger(0);
+//    static AtomicInteger writeLockCount = new AtomicInteger(0);
 
     public static void readLock() {
-        System.out.print("will read lock...\t");
+//        System.out.print("will read lock...\t");
         readWriteLock.readLock().lock();
-        int localCount = localWriteLockCount.get() == null ? 1 : localWriteLockCount.get() + 1;
-        localWriteLockCount.set(localCount);
-        System.out.println("DONE. read lock count:" + lockCount.incrementAndGet() +
-                " this thread read lock count: " + localCount);
-
-        Thread.dumpStack();
+//        int localCount = localWriteLockCount.get() == null ? 1 : localWriteLockCount.get() + 1;
+//        localWriteLockCount.set(localCount);
+//        System.out.println("DONE. read lock count:" + lockCount.incrementAndGet() +
+//                " this thread read lock count: " + localCount);
+//
+//        Thread.dumpStack();
     }
 
     public static void readUnlock() {
         readWriteLock.readLock().unlock();
-        int localCount = localWriteLockCount.get() - 1;
-        localWriteLockCount.set(localCount);
-        System.out.println("unlocked: " + lockCount.decrementAndGet() +
-                " this thread read lock count: " + localCount);
+//        int localCount = localWriteLockCount.get() - 1;
+//        localWriteLockCount.set(localCount);
+//        System.out.println("unlocked: " + lockCount.decrementAndGet() +
+//                " this thread read lock count: " + localCount);
     }
 
     public static void writeLock() {
-        Thread.dumpStack();
-        System.out.print("Will write-lock read lock count: : " + lockCount.get() +
-                " this thread read lock count: " + localWriteLockCount.get());
-        System.out.println("\twrite lock count: " + writeLockCount.get());
+//        Thread.dumpStack();
+//        System.out.print("Will write-lock read lock count: : " + lockCount.get() +
+//                " this thread read lock count: " + localWriteLockCount.get());
+//        System.out.println("\twrite lock count: " + writeLockCount.get());
         readWriteLock.writeLock().lock();
-        System.out.println("in the lock");
+//        System.out.println("in the lock");
 
     }
 
     public static void writeUnlock() {
-        Integer localWriteLocks = localWriteLockCount.get() - 1;
-        localWriteLockCount.set(localWriteLocks);
-        System.out.println("Unlock: " + localWriteLocks);
-        System.out.println("write lock count: " + writeLockCount.decrementAndGet());
+//        Integer localWriteLocks = localWriteLockCount.get() - 1;
+//        localWriteLockCount.set(localWriteLocks);
+//        System.out.println("Unlock: " + localWriteLocks);
+//        System.out.println("write lock count: " + writeLockCount.decrementAndGet());
         readWriteLock.writeLock().unlock();
     }
 }
